971db93954fcc3dd453a74e92e548c6a3625d248
KirillShytov <sitovk587@gmail.com>
2024-01-16 20:20:43 +0200

    ссс

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -11,4 +11,29 @@ function onFormSubmit(e) {
   const { state, delay } = form.elements;
   const selectedState = Array.from(state).find(radio => radio.checked);
   console.log(selectedState);
+  getPromise(delay.value, selectedState.value)
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+}
+
+function getPromise(delay, selectedState) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (selectedState === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
 }
